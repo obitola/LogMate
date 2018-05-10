497371cbb31f0dc8e4c96ee10ac8783770e21330
Tobi Ola <oluwatobi.ola@gmail.com>
2018-05-10 17:11:26 -0500

    added bootstrap template

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./public/routes/index');
+//var usersRouter = require('./public/routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'public'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'LogMate' });
+});
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +40,13 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
+
+
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 module.exports = app;
