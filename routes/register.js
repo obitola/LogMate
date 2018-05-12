4b96f43926103bc449ed86aa55bcecfa0d1a2bd6
Tobi Ola <oluwatobi.ola@gmail.com>
2018-05-12 00:35:41 -0500

    correctly stores info when registering

--- routes/register.js
+++ routes/register.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+let expressValidator = require('express-validator');
+
+router.use(expressValidator());
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -15,35 +18,34 @@ db.connect(function(err) {
    console.log('MySql Connected...');
 });
 
-router.post('/register', function(req, res) {
+router.post('/', function(req, res) {
     console.log('worked');
     let email = req.body.email;
     let first_name = req.body.first_name;
     let last_name = req.body.last_name;
     let password = req.body.password;
 
-    req.checkbody('email', 'Email Field is Required!').notEmpty();
-    req.checkbody('email', 'Email is Not Valid!').isEmail();
-    req.checkbody('first_name', 'First Name Field is Required').notEmpty();
-    req.checkbody('last_name', 'Last Name Field is Required').notEmpty();
-    req.checkbody('password', 'First Name Field is Required').notEmpty();
-    req.checkbody('password2', 'Passwords Do Not Match!').equals(req.body.password);
+    req.assert('email', 'Email Field is Required!').notEmpty();
+    req.assert('email', 'Email is Not Valid!').isEmail();
+    req.assert('first_name', 'First Name Field is Required').notEmpty();
+    req.assert('last_name', 'Last Name Field is Required').notEmpty();
+    req.assert('password', 'First Name Field is Required').notEmpty();
+    req.assert('password2', 'Passwords Do Not Match!').equals(req.body.password);
 
     let errors = req.validationErrors();
 
     if (errors) {
-        console.log(errors);
+        res.render('register', {
+            errors: errors
+        });
     } else {
-        let info = {email: 'oluwatobi.ola@gmail.com', first_name: 'Tobi', last_name: 'Ola', password: 'qwerty'};
+        let info = {email: req.body.email, first_name: req.body.first_name, last_name: req.body.last_name, password: req.body.password};
         let sql = 'INSERT INTO users SET ?';
         let query = db.query(sql, info, function(err, result) {
             if (err) throw err;
-            console.log(result);
-            res.send()
+            res.render('dashboard');
         });
-    }
-
-    
+    }    
 });
 
 router.get('/createuserstable', function(req, res) {
@@ -56,7 +58,7 @@ router.get('/createuserstable', function(req, res) {
 });
 
 /* GET home page. */
-router.get('/register', function(req, res, next) {
+router.get('/', function(req, res, next) {
     res.render('register', { title: 'Express' });
 });
 
