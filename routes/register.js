43d47d15e680f745c72a4ac9dbe9e485c535eeb9
Tobi Ola <oluwatobi.ola@gmail.com>
2018-05-11 22:44:01 -0500

    added post function to register

--- routes/register.js
+++ routes/register.js
@@ -5,31 +5,58 @@ var mysql = require('mysql');
 var db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "123456",
-    socketPath: '/var/run/mysqld/mysqld.sock'
+    password: "",
+    database: 'logmate'
 });
 
-//db.connect(function(err) {
-//    if (err) throw err;
-//    console.log('MySql Connected...');
-//});
+db.connect(function(err) {
+   if (err) throw err;
+   console.log('MySql Connected...');
+});
 
-/* router.get('/createdb', function(req, res) {
-    let sql = 'CREATE DATABASE logmate';
+router.post('/newuser', function(req, res) {
+    console.log('worked');
+    let email = req.body.email;
+    let first_name = req.body.first_name;
+    let last_name = req.body.last_name;
+    let password = req.body.password;
+
+    req.checkbody('email', 'Email Field is Required!').notEmpty();
+    req.checkbody('email', 'Email is Not Valid!').isEmail();
+    req.checkbody('first_name', 'First Name Field is Required').notEmpty();
+    req.checkbody('last_name', 'Last Name Field is Required').notEmpty();
+    req.checkbody('password', 'First Name Field is Required').notEmpty();
+    req.checkbody('password2', 'Passwords Do Not Match!').equals(req.body.password);
+
+    let errors = req.validationErrors();
+
+    if (errors) {
+        console.log(errors);
+    } else {
+        let info = {email: 'oluwatobi.ola@gmail.com', first_name: 'Tobi', last_name: 'Ola', password: 'qwerty'};
+        let sql = 'INSERT INTO users SET ?';
+        let query = db.query(sql, info, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            res.send()
+        });
+    }
+
+    
+});
+
+router.get('/createuserstable', function(req, res) {
+    let sql = 'CREATE TABLE users(user_id int AUTO_INCREMENT, email VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255), password VARCHAR(255), PRIMARY KEY user_id)';
     db.query(sql, function(err, result) {
         if (err) throw err;
         console.log(result);
-        res.send('Database created...');
+        res.send('Table created...');
     });
-}); */
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('register', { title: 'Express' });
-});
-
-router.post('/', function(req, res, next) {
-    
 });
 
 module.exports = router;
